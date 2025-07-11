931d21647070280914e4e189391ed8f6d03878ad
Davide Iaiunese <davide.iaiunese@gmail.com>
2025-07-11 21:07:18 +0200

    refactor(day4): inverse count logic

--- years/2024/day04/index.js
+++ years/2024/day04/index.js
@@ -7,21 +7,20 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export function part1() {
   const lines = readLines(path.join(__dirname, "input.txt"));
-  const xmasCount = findXmasCount(lines);
-  const invertedLines = lines.map((line) => line.split("").reverse().join(""));
-  const invertedXmasCount = findXmasCount(invertedLines);
-  const topDownXmasCount = findVerticalXmasCount(lines);
-  const bottomUPXmasCount = findVerticalXmasCount(invertedLines, true);
+  const horizontalXmasCount = findXmasCount(lines);
+  const verticalXmasCount = findVerticalXmasCount(lines);
 
-  return xmasCount + invertedXmasCount + topDownXmasCount + bottomUPXmasCount;
+  return horizontalXmasCount + verticalXmasCount;
 }
 
 function findXmasCount(lines) {
   let xmasCount = 0;
   for (const line of lines) {
     const pattern = /XMAS/gi;
+    const reversePattern = /SAMX/gi; 
     const matches = line.match(pattern);
-    xmasCount += matches ? matches.length : 0;
+    const inverseMatches = line.match(reversePattern);
+    xmasCount += (matches ? matches.length : 0) + (inverseMatches ? inverseMatches.length : 0);
   }
   return xmasCount;
 }
@@ -29,31 +28,32 @@ function findXmasCount(lines) {
 function findVerticalXmasCount(lines, isInverted = false) {
   let verticalLines = "";
 
-  if (isInverted) {
-    let lineIndexToUse = 0;
-    for (let lineNumber = lines.length - 1; lineNumber >= 0; lineNumber--) {
-      //note: print line char corresponding in increasing order
-      //      es. start from bottom line and go up
-      //      first get all chars at index 0 of each line
-      //      then get all chars at index 1 of each line
-      //      and so on until the end of the line
-      //      right now i'm iterating only on the lines
-      console.log("proceessing line number:", lineNumber);
-      console.log("line[lineIndexToUse]:", lines[lineNumber][lineIndexToUse]);
-      const bottomUpLine = lines.map((line) => line[lineIndexToUse]).join("");
-      verticalLines += bottomUpLine;
-      lineIndexToUse++;
-    }
-  } else {
-    for (let i = 0; i < lines.length; i++) {
-      const topDownLine = lines.map((line) => line[i]).join("");
-      verticalLines += topDownLine;
-      // console.log("Top-down line:", topDownLine);
-    }
+  // if (isInverted) {
+  //   let lineIndexToUse = 0;
+  //   for (let lineNumber = lines.length - 1; lineNumber >= 0; lineNumber--) {
+  //     // note: print line char corresponding in increasing order
+  //     //       es. start from bottom line and go up
+  //     //       first get all chars at index 0 of each line
+  //     //       then get all chars at index 1 of each line
+  //     //       and so on until the end of the line
+  //     //       right now i'm iterating only on the lines
+  //     console.log("lineNumber:", lineNumber);
+  //     console.log("line[lineIndexToUse]:", lines[lineNumber][lineIndexToUse]);
+  //     const bottomUpLine = lines.map((line) => line[lineIndexToUse]).join("");
+  //     verticalLines += bottomUpLine;
+  //     lineIndexToUse++;
+  //   }
+  // } else {
+  // }
+
+  for (let i = 0; i < lines.length; i++) {
+    const topDownLine = lines.map((line) => line[i]).join("");
+    verticalLines += topDownLine;
   }
 
   const topDownLinesCount = verticalLines.match(/XMAS/gi).length || 0;
-  return topDownLinesCount;
+  const bottomUpLinesCount = verticalLines.match(/SAMX/gi).length || 0;
+  return topDownLinesCount + bottomUpLinesCount;
 }
 
 export function part2() {
