4dd4df1efb96c2aefbefbaaab8b0c5733393544c
Davide Iaiunese <davide.iaiunese@gmail.com>
2025-08-03 16:57:44 +0200

    feat(day10): add template

---
+++ years/2024/day10/index.js
@@ -0,0 +1,17 @@
+import { readInput, readLines } from '../../../utils/index.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export function part1() {
+    const lines = readLines(path.join(__dirname, 'input.txt'));
+    console.log('lines', lines);    
+    return 'TODO';
+}
+
+export function part2() {
+    const lines = readLines(path.join(__dirname, 'input.txt'));
+    console.log('lines', lines);    
+    return 'TODO';
+}
