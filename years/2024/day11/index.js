480315303795a1a056db5fa814886c9752f245e3
Davide Iaiunese <davide.iaiunese@gmail.com>
2025-08-31 13:04:23 +0200

    feat(day11): add part 1 solution

--- years/2024/day11/index.js
+++ years/2024/day11/index.js
@@ -1,8 +1,4 @@
-import {
-  convertArrayOfStringsToArrayOfNumbers,
-  readInput,
-  readLines,
-} from "../../../utils/index.js";
+import { readLines } from "../../../utils/index.js";
 import path from "path";
 import { fileURLToPath } from "url";
 
@@ -13,40 +9,48 @@ const convertedInput = lines[0].split(" ").map((x) => parseInt(x, 10));
 
 export function part1() {
   console.log(lines[0].split(" "));
-  console.log(convertedInput);
-  const stones = getSones(convertedInput, blinks);
+  console.log("convertedInput", convertedInput);
+  const stones = getStones(convertedInput, blinks);
+  return stones.length;
 }
 
-function getStones(input, blink) {
-  const stones = [];
-  for (let i = 0; i < input.length; i++) {
-    const stonesForCurrentBlink = getStonesForBlink(input[i], blink);
+function getStones(input, blinks) {
+  let stonesResult = input;
+  for (let i = 1; i <= blinks; i++) {
+    stonesResult = getStonesForBlink(stonesResult).flat();
+    console.log(stonesResult);
   }
+  return stonesResult;
 }
 
-function getStonesForBlink(input, blink) {
+function getStonesForBlink(input) {
+  // console.log("inside getStonesForBlink function");
+  // console.log("input", input);
   const updateStoreLine = input.map((stone) => {
+    // console.log("====================");
+    // console.log("processing stone", stone);
     if (stone === 0) {
+      // console.log("stone is 0, returning 1");
       return 1;
     } else if (stone.toString().length % 2 === 0) {
-      //TODO: If the stone is engraved with a number that has an even number of digits,
-      // it is replaced by two stones.
-      // The left half of the digits are engraved on the new left stone,
-      // and the right half of the digits are engraved on the new right stone.
-      // (The new numbers don't keep extra leading zeroes: 1000 would become stones 10 and 0.)
-
+      // console.log("stone has even number of digits");
+      // console.log("number of digits", stone.toString().length);
       const stonePair = [];
       const stoneString = stone.toString();
-      const digitsFirstHalf = stoneString.slice(0, stoneString.length / 2);
-      const digitsSecondHalf = stoneString.slice(stoneString.length / 2);
-      stonePair.push(
-        parseInt(digitsFirstHalf, 10),
-        parseInt(digitsSecondHalf, 10)
-      );
+      const lefttHalf = stoneString.slice(0, stoneString.length / 2);
+      const rightHalf = stoneString.slice(stoneString.length / 2);
+      // remove any leading zeroes from
+      rightHalf.length > 1 ? rightHalf.replace(/^0+/, "") : rightHalf;
+      // console.log("lefttHalf", lefttHalf);
+      // console.log("rightHalf", rightHalf);
+      stonePair.push(parseInt(lefttHalf, 10), parseInt(rightHalf, 10));
+      // console.log("stonePair", stonePair);
+      return stonePair;
     } else {
       return stone * 2024;
     }
   });
+  // console.log("updateStoreLine", updateStoreLine);
   return updateStoreLine;
 }
 
