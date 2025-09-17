9e66a34932f85f568297a78b31a32400e6c6db85
Davide Iaiunese <davide.iaiunese@gmail.com>
2025-09-17 23:35:30 +0200

    fea(day12): small improvement but not yet there

--- years/2024/day12/index.js
+++ years/2024/day12/index.js
@@ -56,6 +56,7 @@ function closeRegion(char) {
 
 function createRegion(char, perimeter) {
   console.log(`creating region for char ${char}`);
+
   const region = {
     char,
     area: 1,
@@ -63,13 +64,26 @@ function createRegion(char, perimeter) {
     isClosed: false,
   };
 
+  regions = regions.map((r) => {
+    if (r.char === char) {
+      return {
+        ...r,
+        isClosed: true,
+      };
+    } else {
+      return r;
+    }
+  });
+
   console.log(region);
 
   regions.push(region);
 }
 
 function updateRegion(char, perimeter) {
-  console.log(`updateRegion function called with char ${char} whose perimeter is ${perimeter}`);
+  console.log(
+    `updateRegion function called with char ${char} whose perimeter is ${perimeter}`
+  );
   regions = regions.map((r) => {
     if (r.char === char && !r.isClosed) {
       // update matching region
@@ -80,7 +94,7 @@ function updateRegion(char, perimeter) {
         area: r.area + 1,
         perimeter: r.perimeter + perimeter,
       };
-      console.log("updatedRegion", updatedRegion)
+      console.log("updatedRegion", updatedRegion);
       return updatedRegion;
     } else {
       return r;
@@ -92,19 +106,19 @@ function getCharPerimeter(char, charAbove, charAfter, charBelow, charBefore) {
   // Perimeter increases by 1 for each side when the adjacent char is different
   let perimeter = 0;
 
-  if(!charBefore || charBefore !== char){
+  if (!charBefore || charBefore !== char) {
     perimeter++;
   }
 
-  if(!charAbove || charAbove !== char) {
+  if (!charAbove || charAbove !== char) {
     perimeter++;
   }
 
-  if(!charAfter || charAfter !== char) {
+  if (!charAfter || charAfter !== char) {
     perimeter++;
   }
 
-  if(!charBelow || charBelow !== char) {
+  if (!charBelow || charBelow !== char) {
     perimeter++;
   }
 
@@ -114,7 +128,7 @@ function getCharPerimeter(char, charAbove, charAfter, charBelow, charBefore) {
 }
 
 export function part1() {
-  //TODO: implement region closing logic to avoid putting in same region when in reality 
+  //TODO: implement region closing logic to avoid putting in same region when in reality
   // should create new region
 
   const rawLines = readLines(path.join(__dirname, "input.txt"));
@@ -125,26 +139,26 @@ export function part1() {
     console.log("===============================");
     console.log("line", lines[i]);
     let char,
-    charBefore,
-    charAfter,
-    charBelow,
-    charAbove,
+      charBefore,
+      charAfter,
+      charBelow,
+      charAbove,
       charPerimiter = undefined;
-    
+
     // if line is the first one
     //   - check if a region for same char exisits
     //   - if it does exist
     //      add this char to that region count
     //   - else
     //      create region for that char
-    
+
     // iterate over each char in the line
     for (let j = 0; j < lines[i].length; j++) {
       char = lines[i][j];
-      charAbove = i > 0 ? lines[i-1][j] : undefined;
-      charAfter = j < lines[i].length - 1 ? lines[i][j+1] : undefined;
-      charBelow = i < lines.length - 1 ? lines[i+1][j] : undefined;
-      charBefore = j > 0 ? lines[i][j-1] : undefined;
+      charAbove = i > 0 ? lines[i - 1][j] : undefined;
+      charAfter = j < lines[i].length - 1 ? lines[i][j + 1] : undefined;
+      charBelow = i < lines.length - 1 ? lines[i + 1][j] : undefined;
+      charBefore = j > 0 ? lines[i][j - 1] : undefined;
 
       // console.log("char", char);
       // console.log("charAbove", charAbove);
@@ -154,9 +168,15 @@ export function part1() {
 
       console.log("-------------------------------");
       console.log(`char ${char} at line ${i}, index ${j}`);
-      
+
       if (i == 0) {
-        charPerimiter = getCharPerimeter(char, charAbove, charAfter, charBelow, charBefore);
+        charPerimiter = getCharPerimeter(
+          char,
+          charAbove,
+          charAfter,
+          charBelow,
+          charBefore
+        );
         if (getRegion(char)) {
           updateRegion(char, charPerimiter);
         } else {
@@ -164,10 +184,16 @@ export function part1() {
         }
       } else {
         char = lines[i][j];
-        charPerimiter = getCharPerimeter(char, charAbove, charAfter, charBelow, charBefore);
+        charPerimiter = getCharPerimeter(
+          char,
+          charAbove,
+          charAfter,
+          charBelow,
+          charBefore
+        );
         // if the same letter is in the cell above or on the left
         // it means we are in an existing region, so we can update it
-        if (char === charAbove || (j > 0 && char == lines[i][j-1])) {
+        if (char === charAbove || char === charBefore) {
           updateRegion(char, charPerimiter);
         } else {
           createRegion(char, charPerimiter);
@@ -176,7 +202,6 @@ export function part1() {
       console.log("regions", regions);
     }
   }
-
 
   // Now, we have all the regions
   // So for each region
@@ -187,7 +212,7 @@ export function part1() {
 
   let total = 0;
 
-  regions.forEach(r => {
+  regions.forEach((r) => {
     total += r.area * r.perimeter;
   });
 
