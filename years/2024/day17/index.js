74393b97c3962bcea9415d6ce975ecebfc0de5f7
Davide Iaiunese <davide.iaiunese@gmail.com>
2025-12-08 22:15:17 +0100

    refactor(day17): add more foundations code

--- years/2024/day17/index.js
+++ years/2024/day17/index.js
@@ -1,5 +1,5 @@
 import { readInput, readLines } from "../../../utils/index.js";
-import path from "path";
+import path, { parse } from "path";
 import { fileURLToPath } from "url";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
@@ -18,7 +18,8 @@ export function solution() {
    */
 
   function adv(opCode, operand) {
-    return opCode / operand;
+    const denominator = 2 ^ operand;
+    return opCode / denominator;
   }
 
   function bxl(registerB, instructionLiteralOperand) {
@@ -60,6 +61,19 @@ export function solution() {
     registerB = adv(opCode, operand);
   }
 
+  function getOperand(input) {
+    switch (input) {
+      case 4:
+        return registerA;
+      case 5:
+        return registerB;
+      case 6:
+        return registerC;
+      default:
+        return input;
+    }
+  }
+
   const lines = readLines(path.join(__dirname, "input.txt"));
   console.log(lines);
   let registerA = parseInt(lines[0].split(": ")[1]);
@@ -69,8 +83,51 @@ export function solution() {
   console.log("Register A:", registerA);
   console.log("Register B:", registerB);
   console.log("Register C:", registerC);
-  const instructions = lines[4];
+  const instructionsLine = lines[4];
+  const instructions = instructionsLine
+    .split(": ")[1]
+    .split(",")
+    .map((x) => x.trim())
+    .map((x) => parseInt(x));
   console.log("Instructions:", instructions);
+
+  for (let i = 0; i < instructions.length; i += 2) {
+    const opCode = instructions[i];
+    const operand = getOperand(instructions[i + 1]);
+    console.log("OpCode:", opCode, "Operand:", operand);
+    switch (opCode) {
+      case 0:
+        console.log("case 0, Executing BXL");
+        bxl(registerB, operand);
+        break;
+      case 1:
+        console.log("case 1, Executing BST");
+        bst(parseInt(operand));
+        break;
+      case 2:
+        console.log("case 2, Executing JNZ");
+        jnz(registerA);
+        break;
+      case 3:
+        console.log("case 3, Executing BXC");
+        bxc(registerB, registerC);
+        break;
+      case 4:
+        console.log("case 4, Executing OUT");
+        out();
+        break;
+      case 5:
+        console.log("case 5, Executing BDV");
+        bdv(registerB, operand);
+        break;
+      case 6:
+        console.log("case 6, Executing CDV");
+        cdv(registerC, operand);
+        break;
+    }
+  }
+
+  console.log("Output results:", outRsesults);
 
   return "TODO";
 }
