d50562c24cf0c89bee3d9f9f6c1b5cab946ba787
Davide Iaiunese <davide.iaiunese@gmail.com>
2025-12-15 09:17:08 +0100

    docs(day17): add note

--- years/2024/day17/index.js
+++ years/2024/day17/index.js
@@ -1,11 +1,17 @@
-import { get } from "http";
-import { readInput, readLines } from "../../../utils/index.js";
+import { readLines } from "../../../utils/index.js";
 import path, { parse } from "path";
 import { fileURLToPath } from "url";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const lines = readLines(path.join(__dirname, "input.txt"));
+const instructionsLine = lines[4];
+const instructions = instructionsLine
+  .split(": ")[1]
+  .split(",")
+  .map((x) => x.trim())
+  .map((x) => parseInt(x));
 
-export function solution() {
+export function run(regA, regB, regC) {
   /**
    * - Extract input
    *    - extract register values
@@ -19,15 +25,8 @@ export function solution() {
    */
 
   function dv(numerator, operand) {
-    console.log(
-      "Calculating DV with numerator:",
-      numerator,
-      "and operand:",
-      operand
-    );
     const denominator = 2 ** operand;
     const result = numerator / denominator;
-    console.log("result of dv:", result);
     return result;
   }
 
@@ -73,103 +72,89 @@ export function solution() {
   function getOperand(input) {
     switch (input) {
       case 4:
-        console.log(
-          "Since input is 4, returning register A as operand, which is:",
-          registerA
-        );
         return registerA;
       case 5:
-        console.log(
-          "Since input is 5, returning register B as operand, which is:",
-          registerB
-        );
         return registerB;
       case 6:
-        console.log(
-          "Since input is 6, returning register C as operand, which is:",
-          registerC
-        );
         return registerC;
       default:
-        console.log(`since input is ${input}, returning it as operand`);
         return input;
     }
   }
 
-  const lines = readLines(path.join(__dirname, "input.txt"));
-  console.log(lines);
-  let registerA = parseInt(lines[0].split(": ")[1]);
-  let registerB = parseInt(lines[1].split(": ")[1]);
-  let registerC = parseInt(lines[2].split(": ")[1]);
+  let registerA = regA;
+  let registerB = regB;
+  let registerC = regC;
   const outRsesults = [];
-  console.log("Register A:", registerA);
-  console.log("Register B:", registerB);
-  console.log("Register C:", registerC);
-  const instructionsLine = lines[4];
-  const instructions = instructionsLine
-    .split(": ")[1]
-    .split(",")
-    .map((x) => x.trim())
-    .map((x) => parseInt(x));
-  console.log("Instructions:", instructions);
 
   for (let i = 0; i < instructions.length; i += 2) {
-    console.log("Processing instruction at index:", i);
-    console.log(
-      "Current Registers - A:",
-      registerA,
-      "B:",
-      registerB,
-      "C:",
-      registerC
-    );
     const opCode = instructions[i];
     let operand = getOperand(instructions[i + 1]);
     switch (opCode) {
       case 0:
-        console.log("case 0, Executing ADV");
         registerA = adv(registerA, operand);
         break;
       case 1:
-        console.log("case 1, Executing BXL");
         operand = instructions[i + 1];
         registerB = bxl(registerB, operand);
         break;
       case 2:
-        console.log("case 2, Executing BST");
         registerB = bst(operand);
         break;
       case 3:
-        console.log("case 3, Executing JNZ");
         operand = instructions[i + 1];
         if (registerA !== 0) {
-          console.log("current operand is:", operand);
-          console.log("current i:", i);
-          console.log("setting i to:", operand - 2);
-          console.log("new i will be:", operand - 2);
           i = operand - 2;
         }
         break;
       case 4:
-        console.log("case 4, Executing BXC");
         registerB = bxc(registerB, registerC);
         break;
       case 5:
-        console.log("case 5, Executing OUT");
         outRsesults.push(out(operand));
         break;
       case 6:
-        console.log("case 6, Executing BDV");
         registerB = bdv(registerA, operand);
         break;
       case 7:
-        console.log("case 7, Executing CDV");
         registerC = cdv(registerA, operand);
         break;
     }
   }
 
-  console.log("Output results:", outRsesults);
-
   return outRsesults.join(",");
+}
+
+export function part1() {
+  let registerA = parseInt(lines[0].split(": ")[1]);
+  let registerB = parseInt(lines[1].split(": ")[1]);
+  let registerC = parseInt(lines[2].split(": ")[1]);
+  return run(registerA, registerB, registerC);
+}
+
+export function part2() {
+  let registerA = parseInt(lines[0].split(": ")[1]);
+  let registerB = parseInt(lines[1].split(": ")[1]);
+  let registerC = parseInt(lines[2].split(": ")[1]);
+
+  // In part 1 the program loops 6 times, and
+  // each iteration it divides register A by 8
+  // Strategy:
+  // - calc min and max attempts to reach 0 in register A
+  // - brute force program executiiton with register A values in that range
+
+  const minAttempts = 8 ** 15;
+  const maxAttempts = 8 ** 16;
+
+  //TODO:
+  // this brute force strategy doesnt work for very large ranges...
+  // Need to find a better way
+  for (let i = minAttempts; i <= maxAttempts; i++) {
+    const result = run(i, registerB, registerC);
+    if (result === instructions.join(",")) {
+      return i;
+    }
+  }
+
+  return "No solution found";
 }
