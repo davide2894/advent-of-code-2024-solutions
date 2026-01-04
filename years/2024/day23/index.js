8342d7ca68c8b2f88191f36659b50bd6045c27a0
Davide Iaiunese <davide.iaiunese@gmail.com>
2026-01-04 21:27:15 +0100

    feat(day23): add part 2 solution

--- years/2024/day23/index.js
+++ years/2024/day23/index.js
@@ -3,36 +3,30 @@ import path from "path";
 import { fileURLToPath } from "url";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const lines = readLines(path.join(__dirname, "input.txt"));
+const adj = new Map();
+
+for (const line of lines) {
+  const [a, b] = line.split("-");
+  if (!adj.has(a)) {
+    adj.set(a, new Set());
+  }
+  if (!adj.has(b)) {
+    adj.set(b, new Set());
+  }
+  adj.get(a).add(b);
+  adj.get(b).add(a);
+}
 
 export function part1() {
-  const lines = readLines(path.join(__dirname, "input.txt"));
-  console.log("lines", lines);
-  const adj = new Map();
   const triangles = new Set();
   let count = 0;
-
-  for (const line of lines) {
-    const [a, b] = line.split("-");
-    if (!adj.has(a)) {
-      adj.set(a, new Set());
-    }
-    if (!adj.has(b)) {
-      adj.set(b, new Set());
-    }
-    adj.get(a).add(b);
-    adj.get(b).add(a);
-  }
-
-  console.log("adj", adj);
-
   const nodes = Array.from(adj.keys());
-  console.log("nodes", nodes);
 
   for (let i = 0; i < nodes.length; i++) {
     for (let j = i + 1; j < nodes.length; j++) {
       const a = nodes[i];
       const b = nodes[j];
-      console.log("checking", a, b);
       // check if edge exists between a and b
       if (adj.get(a).has(b)) {
         // look for common neightbors
@@ -56,4 +50,35 @@ export function part1() {
   }
 
   return count;
+}
+
+export function part2() {
+  // 1. find largest set of computers that are all connected to each other
+  // 2. return the password, which is the concatenation of their names in alphabetical order
+  console.log("adj", adj);
+  const nodes = Array.from(adj.keys());
+  console.log("nodes", nodes);
+  let largestClique = [];
+
+  for (let i = 0; i < nodes.length; i++) {
+    const currentClique = [nodes[i]];
+    // check all pairings of current nodes and its neighbors
+    for (let j = i + 1; j < nodes.length; j++) {
+      // check if neightbor node is connected to all nodes in clique
+      const neightbor = nodes[j];
+      let isConnectedToAll = currentClique.every((cliqueNode) =>
+        adj.get(cliqueNode).has(neightbor)
+      );
+      if (isConnectedToAll) {
+        currentClique.push(neightbor);
+      }
+    }
+    if (currentClique.length > largestClique.length) {
+      largestClique = currentClique;
+    }
+  }
+
+  console.log("largestClique", largestClique);
+
+  return largestClique.sort().join(",");
 }
